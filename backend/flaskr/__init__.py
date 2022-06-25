47ce6e7db764376d47b9900b3a7706d2497d2a35
Obinna <ugwusolomonobinna@gmail.com>
2022-06-25 22:02:56 +0100

    updated endpoints and error handlers

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -141,6 +141,14 @@ def create_app(test_config=None):
     TEST: When you submit a question on the "Add" tab,
     the form will clear and the question will appear at the end of the last page
     of the questions list in the "List" tab.
+    
+    Create a POST endpoint to get questions based on a search term.
+    It should return any questions for whom the search term
+    is a substring of the question.
+
+    TEST: Search by any phrase. The questions list will update to include
+    only question that include that string within their question.
+    Try using the word "title" to start.
     """
     @app.route('/questions', methods=['POST'])
     def new_question():
@@ -181,18 +189,7 @@ def create_app(test_config=None):
             })
         except:
             abort(422)
-
-
-    """
-    @TODO:
-    Create a POST endpoint to get questions based on a search term.
-    It should return any questions for whom the search term
-    is a substring of the question.
-
-    TEST: Search by any phrase. The questions list will update to include
-    only question that include that string within their question.
-    Try using the word "title" to start.
-    """
+    
     
     """
     @TODO:
